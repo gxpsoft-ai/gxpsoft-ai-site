0d92af23996d36218bd336a2000cdac4d646e2b3
Duk Lee <duke@saram.io>
2026-09-18 06:55:40 -0700

    Add Korean (ko) as second language to website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,13 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://gxpsoft.ai',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ko'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
   build: {
     format: 'file',
   },
